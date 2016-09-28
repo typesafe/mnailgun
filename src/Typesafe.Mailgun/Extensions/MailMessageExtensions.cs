9b7a96732e4dcf8518ce5cab16ca83e5763241b3
Simon Dingley <simon@prolificnotion.co.uk>
2016-09-28 11:35:12 +0100

    Adds the ability to apply tags to your email messages for filtering in reports later on.

---
+++ src/Typesafe.Mailgun/Extensions/MailMessageExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Mail;
+
+namespace Typesafe.Mailgun.Extensions
+{
+	/// <summary>
+	/// Extensions to the <see cref="System.Net.Mail.MailMessage"/> class
+	/// </summary>
+	public static class MailMessageExtensions
+	{
+		/// <summary>
+		/// Adds a tag to the <see cref="System.Net.Mail.MailMessage" />.
+		/// </summary>
+		/// <param name="mailMessage">The <see cref="System.Net.Mail.MailMessage" /> instance.</param>
+		/// <param name="tag">The tag to add to the <see cref="System.Net.Mail.MailMessage" />.</param>
+		public static void AddTag(this MailMessage mailMessage, string tag)
+		{
+			mailMessage.Headers.Add("X-Mailgun-Tag", tag);
+		}
+	}
+}
