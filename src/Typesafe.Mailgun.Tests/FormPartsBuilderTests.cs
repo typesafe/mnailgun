7c0955c4dc62fa0d21e41ae8b61aac6021640cef
Simon Dingley <simon@prolificnotion.co.uk>
2016-09-30 16:24:35 +0100

    Adds the ability to apply custom variables to your email messages which can be accessed via your webhooks later on

--- src/Typesafe.Mailgun.Tests/FormPartsBuilderTests.cs
+++ src/Typesafe.Mailgun.Tests/FormPartsBuilderTests.cs
@@ -9,6 +9,8 @@ using Typesafe.Mailgun.Http;
 
 namespace Typesafe.Mailgun.Tests
 {
+	using Typesafe.Mailgun.Extensions;
+
 	[TestFixture]
 	public class FormPartsBuilderTests
 	{
@@ -70,6 +72,15 @@ namespace Typesafe.Mailgun.Tests
 			var message = BuildMessage(x => x.Subject = "Test Passed");
 			var result = FormPartsBuilder.Build(message);
 			result.AssertContains("subject", "Test Passed");
+		}
+
+		[Test]
+		public void Build_VariablesSpecified_AddsVariablesPart()
+		{
+			var message = BuildMessage(x => x.AddVariable("my_message_id", "123"));
+			var result = FormPartsBuilder.Build(message);
+
+			result.AssertContains("v:my_message_id", "123");
 		}
 
 		[Test]
---
+++ src/Typesafe.Mailgun/Extensions/MailMessageExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Mail;
+
+namespace Typesafe.Mailgun.Extensions
+{
+	/// <summary>
+	/// Extensions to the <see cref="System.Net.Mail.MailMessage"/> class
+	/// </summary>
+	public static class MailMessageExtensions
+	{ 
+		public static void AddVariable(this MailMessage mailMessage, string name, string value)
+		{
+			mailMessage.Headers.Add("X-Mailgun-Variables", string.Format("{{\"{0}\": \"{1}\"}}", name, value));
+		}
+	}
+}
