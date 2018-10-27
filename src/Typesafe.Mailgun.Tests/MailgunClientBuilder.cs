853504577c8b986d116d241c64e5b144a22f7419
Mongkon Eiadon <mongkon.eiadon@hotmail.com>
2018-10-27 17:28:23 +0700

    fix credential

--- src/Typesafe.Mailgun.Tests/MailgunClientBuilder.cs
+++ src/Typesafe.Mailgun.Tests/MailgunClientBuilder.cs
@@ -2,9 +2,9 @@
 {
     public static class MailgunClientBuilder
     {
-        public static MailgunClient GetClient(string domain = "www.keenprofile.com")
+        public static MailgunClient GetClient(string domain = "yourdomain")
         {
-            return new MailgunClient(domain, "key-06dfae80d176be561877d48dacc8b4d3", 3);
+            return new MailgunClient(domain, "key-withyourapikey", 3);
         }
     }
 }
